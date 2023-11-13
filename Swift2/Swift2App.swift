e67a04fb00ff672703e5143503fd861112fcc249
Christer Lindqvist <christer@festinafinance.com>
2023-11-13 21:01:10 +0100

    Initial Commit

---
+++ Swift2/Swift2App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift2App.swift
+//  Swift2
+//
+//  Created by Christer Lindqvist on 2023-11-13.
+//
+
+import SwiftUI
+
+@main
+struct Swift2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
