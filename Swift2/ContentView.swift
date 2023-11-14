7dcc570b95d9b81f02a6226e59195e2b30c618ae
Christer Lindqvist <christer@festinafinance.com>
2023-11-14 08:27:27 +0100

    Uppgift 2

--- Swift2/ContentView.swift
+++ Swift2/ContentView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var theList = [""]
+    @State var name = ""
+    
     var body: some View {
+   
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Spacer()
+                TextField("", text: $name )
+                    
+                Button( action: {
+                    if ( name != "") {
+                        theList.append(name)
+                        name = ""
+                    }
+                }) {
+                    Text("Lägg till")
+                }
+                Spacer()
+            }
+
+            Spacer()
+            
+            List(theList, id:  \.self ) { aPerson in
+                 personListView(name: aPerson)
+             }
+            
+            Spacer()
+            
+            Button( action: {
+                theList.removeAll()
+            }) {
+                Text("Nollställ")
+            }
         }
         .padding()
     }
---
+++ Swift2/personListView.swift
@@ -0,0 +1,19 @@
+//
+//  personListView.swift
+//  Swift2
+//
+//  Created by Christer Lindqvist on 2023-11-13.
+//
+
+import SwiftUI
+
+struct personListView: View {
+    var name = "VADC"
+    var body: some View {
+        Text(name)
+    }
+}
+
+#Preview {
+    personListView()
+}
